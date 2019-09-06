e45e23358ab7f7d0990f756e64f2d189769f4861
Clint Kennedy <clint.kennedy@protolabs.com>
2019-09-06 15:28:40 -0500

    Add custom handlers from previous version

--- demo/wsgi.py
+++ demo/wsgi.py
@@ -13,6 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from .actions.headline_actions import *
 from .actions.incident_commands import *
 from .actions.incident_notifications import *
+from .actions.keyword_handlers import *
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo.settings.dev')
 
