da08a056c8d808b0f7602e2e9f4df349d42f2ccb
Clint Kennedy <clint.kennedy@protolabs.com>
2019-09-06 14:39:54 -0500

    Modify startup to leverage username and password from env file.

--- startup.sh
+++ startup.sh
@@ -14,7 +14,7 @@ from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 User = get_user_model()
 try:
-    User.objects.create_superuser('admin', '', 'admin')
+    User.objects.create_superuser('$ADMIN_USER', '', '$ADMIN_PASS')
 except IntegrityError:
     pass
 EOF
