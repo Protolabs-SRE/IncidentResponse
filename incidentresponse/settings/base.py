5331b1a68372cf56d5b681eaec119d35f26e21a5
Ashley Poole <git@ashleypoole.co.uk>
2019-10-08 15:29:54 +0100

    Disable response login required for views and bump version

--- incidentresponse/settings/base.py
+++ incidentresponse/settings/base.py
@@ -121,9 +121,11 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ]
 }
+
+RESPONSE_LOGIN_REQUIRED = False
 
 # Markdown Filter
 
